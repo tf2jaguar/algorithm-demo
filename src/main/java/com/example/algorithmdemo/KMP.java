7d3c1e03573d52761752dfdacb293c1105332e4f
guodongzhang <jelly_54@163.com>
2021-08-25 07:27:05 +0800

    增加kmp备注

--- src/main/java/com/example/algorithmdemo/KMP.java
+++ src/main/java/com/example/algorithmdemo/KMP.java
@@ -4,6 +4,12 @@ package com.example.algorithmdemo;
  * @author ：guodongzhang
  * @date ：Created in 2021/8/24 17:19
  */public class KMP {
+
+    /**
+     * 获取KMP算法中pattern字符串对应的next数组
+     *
+     * @param chars 模式字符串对应的字符数组
+     */
     public int[] getNext(char[] chars) {
         final int len = chars.length;
         int[] next = new int[len];
@@ -32,10 +38,12 @@ package com.example.algorithmdemo;
 
         int i = 0, j = 0;
         while (i < sLen && j < pLen) {
+            // 如果j = -1,或者当前字符匹配成功(src[i] = ptn[j]),都让i++,j++
             if (j == -1 || sChar[i] == pChar[j]) {
                 i++;
                 j++;
             } else {
+                // 如果j!=-1且当前字符匹配失败,则令i不变,j=next[j],即让pattern模式串右移j-next[j]个单位
                 j = next[j];
             }
         }
