d8961633cfac19cb79bf537cf8d860bebcf63308
guodongzhang <jelly_54@163.com>
2021-09-23 20:21:49 +0800

    leetcode.cn-二叉树:98 : 验证二叉搜索树

---
+++ src/main/java/leetcode/editor/cn/TreeNode.java
@@ -0,0 +1,24 @@
+package leetcode.editor.cn;
+
+/**
+ * @author zhangguodong
+ * @date 2021/9/22 15:51
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
