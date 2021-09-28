44df7f32c804875af29a586d539ab34be8c913ac
guodongzhang <jelly_54@163.com>
2021-09-28 11:47:55 +0800

    leetcode.cn-位运算:137 : 只出现一次的数字 II(哈希表)

---
+++ src/main/java/leetcode/editor/cn/SingleNumberIi.java
@@ -0,0 +1,60 @@
+//给你一个整数数组 nums ，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 。请你找出并返回那个只出现了一次的元素。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,2,3,2]
+//输出：3
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [0,1,0,1,0,1,99]
+//输出：99
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 3 * 10⁴ 
+// -2³¹ <= nums[i] <= 2³¹ - 1 
+// nums 中，除某个元素仅出现 一次 外，其余每个元素都恰出现 三次 
+// 
+//
+// 
+//
+// 进阶：你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？ 
+// Related Topics 位运算 数组 👍 727 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * title: 137 : 只出现一次的数字 II
+ * create: 2021-09-28 11:39:54
+ */
+public class SingleNumberIi {
+    public static void main(String[] args) {
+        Solution solution = new SingleNumberIi().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int singleNumber(int[] nums) {
+            Map<Integer, Integer> map = new HashMap<>(nums.length);
+            for (int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+            }
+            return map.keySet().stream().filter(k -> map.get(k) == 1).findFirst().get();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
