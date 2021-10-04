1aa453238121ec3f4de72706156f1a07a291e977
guodongzhang <jelly_54@163.com>
2021-10-04 18:40:54 +0800

    lintcode.com-动态规划:602 · 俄罗斯套娃信封(二分法，参考最长递增子序列)

--- src/main/java/lintcode/com/dynamic/sequence/MaxEnvelopes.java
+++ src/main/java/lintcode/com/dynamic/sequence/MaxEnvelopes.java
@@ -20,9 +20,6 @@
 package lintcode.com.dynamic.sequence;
 
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 602 · 俄罗斯套娃信封
@@ -33,7 +30,10 @@ import java.util.Map;
 public class MaxEnvelopes {
     public static void main(String[] args) {
         MaxEnvelopes me = new MaxEnvelopes();
-        System.out.println(me.maxEnvelopes(new int[][]{{4, 5}, {4, 6}, {6, 7}, {2, 3}, {1, 1}}));
+        System.out.println(me.maxEnvelopes(new int[][]{{4, 5}, {4, 6}, {6, 7}, {2, 3}, {1, 1}}) == 4);
+        System.out.println(me.maxEnvelopes(new int[][]{{5, 4}, {6, 4}, {6, 7}, {2, 3}}) == 3);
+        System.out.println(me.maxEnvelopes(new int[][]{{15, 8}, {2, 20}, {2, 14}, {4, 17}, {8, 19}, {8, 9}, {5, 7}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {8, 11}, {13, 11}, {2, 13}, {11, 19}, {16, 1}, {18, 13}, {14, 17}, {18, 19}}) == 5);
+
     }
 
     /**
@@ -45,25 +45,34 @@ public class MaxEnvelopes {
             return 0;
         }
 
-        Arrays.sort(envelopes, (o1, o2) -> {
-            if (o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            }
-            return o1[0] - o2[0];
-        });
+        /**
+         * 就是当w相同时，h逆序，从大到小排列
+         * 针对h求出来的最长递增子序列不会存在w相等，而h递增的情况，因为w相同的时候，右边的数总是小于等于左边的数，不会出现在最长递增子序列里面
+         */
+        Arrays.sort(envelopes, (o1, o2) -> o1[0] == o2[0] ? o2[1] - o1[1] : o1[0] - o2[0]);
 
         int len = envelopes.length;
+        // 对信封的 高h 求最长递增子序列
         int[] dp = new int[len];
-        int i, j, res = 0;
-        for (i = 0; i < len; i++) {
-            dp[i] = 1;
-            for (j = 0; j < i; j++) {
-                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] > envelopes[j][1]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
+        int size = 0;
+        for (int[] envelope : envelopes) {
+            int h = envelope[1];
+            int l = 0, r = size;
+            while (l < r) {
+                int m = l + ((r - l) >> 1);
+                if (dp[m] < h) {
+                    l = m + 1;
+                } else {
+                    r = m;
                 }
             }
-            res = Math.max(res, dp[i]);
+            dp[l] = h;
+            if (r == size) {
+                size++;
+            }
         }
-        return res;
+
+        return size;
     }
+
 }
