27f15265ca008d3aca7627b223bd37c1d12ba26c
guodongzhang <jelly_54@163.com>
2021-10-06 09:11:01 +0800

    leetcode.cn-二叉树:生成TreeNode方法

--- src/main/java/leetcode/editor/cn/TreeNode.java
+++ src/main/java/leetcode/editor/cn/TreeNode.java
@@ -1,5 +1,10 @@
 package leetcode.editor.cn;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author zhangguodong
  * @date 2021/9/22 15:51
@@ -20,5 +25,57 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    /**
+     * 恢复层序遍历的树
+     */
+    public static TreeNode generate(Integer[] list) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode head = null;
+        int flag = 0;
+        for (Integer s : list) {
+
+            if (flag == 0) {
+                flag = 1;
+                if (s == null) {
+                    break;
+                }
+                head = new TreeNode(s);
+                queue.offer(head);
+            } else if (flag == 1) {
+                if (!queue.isEmpty()) {
+                    TreeNode peek = queue.peek();
+                    if (s == null) {
+                        flag = 2;
+                        continue;
+                    }
+                    TreeNode newLeft = new TreeNode(s);
+
+                    peek.left = newLeft;
+                    queue.offer(newLeft);
+                    flag = 2;
+                }
+            } else if (flag == 2) {
+                if (!queue.isEmpty()) {
+                    TreeNode peek = queue.poll();
+                    if (s == null) {
+                        flag = 1;
+                        continue;
+                    }
+                    TreeNode newRight = new TreeNode(s);
+                    peek.right = newRight;
+                    queue.offer(newRight);
+                    flag = 1;
+                }
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        Integer[] list = new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1};
+        TreeNode generate = TreeNode.generate(list);
+        System.out.println(generate);
     }
 }
