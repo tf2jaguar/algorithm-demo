a1a98986d153de59da936730c07624938c837356
guodongzhang <jelly_54@163.com>
2021-10-06 09:21:14 +0800

    leetcode.cn-二叉树:144 : 二叉树的前序遍历(递归)

--- src/main/java/leetcode/editor/cn/BinaryTreePreorderTraversal.java
+++ src/main/java/leetcode/editor/cn/BinaryTreePreorderTraversal.java
@@ -83,9 +83,9 @@ public class BinaryTreePreorderTraversal {
      */
     class Solution {
         public List<Integer> preorderTraversal(TreeNode root) {
-//            List<Integer> res = new ArrayList<>();
-//            preorder(root, res);
-            return morris(root);
+            List<Integer> res = new ArrayList<>();
+            preorder(root, res);
+            return res;
         }
 
         private void preorder(TreeNode root, List<Integer> res) {
@@ -97,35 +97,6 @@ public class BinaryTreePreorderTraversal {
             preorder(root.right, res);
         }
 
-        public List<Integer> morris(TreeNode root) {
-            List<Integer> res = new ArrayList<Integer>();
-            if (root == null) {
-                return res;
-            }
-
-            TreeNode p1 = root, p2 = null;
-
-            while (p1 != null) {
-                p2 = p1.left;
-                if (p2 != null) {
-                    while (p2.right != null && p2.right != p1) {
-                        p2 = p2.right;
-                    }
-                    if (p2.right == null) {
-                        res.add(p1.val);
-                        p2.right = p1;
-                        p1 = p1.left;
-                        continue;
-                    } else {
-                        p2.right = null;
-                    }
-                } else {
-                    res.add(p1.val);
-                }
-                p1 = p1.right;
-            }
-            return res;
-        }
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
