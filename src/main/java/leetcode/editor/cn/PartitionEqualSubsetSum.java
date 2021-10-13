492402aa78cdd9119d50243430cc6e09a08a2c92
guodongzhang <jelly_54@163.com>
2021-10-13 17:09:06 +0800

    leetcode.cn-动态规划:背包问题:0-1背包-存在性问题:416 : 分割等和子集

---
+++ src/main/java/leetcode/editor/cn/PartitionEqualSubsetSum.java
@@ -0,0 +1,67 @@
+//给你一个 只包含正整数 的 非空 数组 nums 。请你判断是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [1,5,11,5]
+//输出：true
+//解释：数组可以分割成 [1, 5, 5] 和 [11] 。 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [1,2,3,5]
+//输出：false
+//解释：数组不能分割成两个元素和相等的子集。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 200 
+// 1 <= nums[i] <= 100 
+// 
+// Related Topics 数组 动态规划 👍 965 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+
+/**
+ * title: 416 : 分割等和子集
+ * create: 2021-10-13 16:56:02
+ */
+public class PartitionEqualSubsetSum {
+    public static void main(String[] args) {
+        Solution solution = new PartitionEqualSubsetSum().new Solution();
+        System.out.println(solution.canPartition(new int[]{1, 5, 11, 5}));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean canPartition(int[] nums) {
+            int sum = Arrays.stream(nums).sum();
+            // 总和不能是奇数
+            if ((sum & 1) == 1) {
+                return false;
+            }
+            int target = sum >> 1;
+            // dp[i]:是否存在子集和为i
+            boolean[] dp = new boolean[target + 1];
+            // 不选任何元素
+            dp[0] = true;
+            for (int num : nums) {
+                for (int i = target; i >= num; i--) {
+                    dp[i] = dp[i] || dp[i - num];
+                }
+            }
+            return dp[target];
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
