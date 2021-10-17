be62d7f7dea4027b958b7d5f51cc107bff54c1a8
guodongzhang <jelly_54@163.com>
2021-10-17 19:10:47 +0800

    leetcode.cn-字符串:KMP:28 : 实现 strStr()

---
+++ src/main/java/leetcode/editor/cn/ImplementStrstr.java
@@ -0,0 +1,106 @@
+//实现 strStr() 函数。 
+//
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如
+//果不存在，则返回 -1 。 
+//
+// 
+//
+// 说明： 
+//
+// 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。 
+//
+// 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：haystack = "hello", needle = "ll"
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：haystack = "aaaaa", needle = "bba"
+//输出：-1
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：haystack = "", needle = ""
+//输出：0
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= haystack.length, needle.length <= 5 * 10⁴ 
+// haystack 和 needle 仅由小写英文字符组成 
+// 
+// Related Topics 双指针 字符串 字符串匹配 👍 1005 👎 0
+
+package leetcode.editor.cn;
+
+/**
+ * title: 28 : 实现 strStr()
+ * create: 2021-08-25 07:30:04
+ */
+public class ImplementStrstr {
+    public static void main(String[] args) {
+        Solution solution = new ImplementStrstr().new Solution();
+        String a = "ababaabbbbababbaabaaabaabbaaaabbabaabbbbbbabbaabbabbbabbbbbaaabaababbbaabbbabbbaabbbbaaabbababbabbbabaaabbaabbabababbbaaaaaaababbabaababaabbbbaaabbbabb";
+        String b = "abbabbbabaa";
+        System.out.println(solution.strStr(a, b));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int strStr(String haystack, String needle) {
+            if (needle == null || needle.length() == 0) {
+                return 0;
+            }
+            int[] next = getNext(needle);
+            int j = 0;
+            for (int i = 0; i < haystack.length(); i++) {
+                while (j > 0 && haystack.charAt(i) != needle.charAt(j)) {
+                    j = next[j - 1];
+                }
+
+                if (haystack.charAt(i) == needle.charAt(j)) {
+                    j++;
+                }
+                if (j == needle.length()) {
+                    return (i - needle.length() + 1);
+                }
+            }
+            return -1;
+        }
+
+        /**
+         * 获取KMP算法中pattern字符串对应的next数组
+         *
+         * @param s 模式字符串对应的字符数组
+         */
+        public int[] getNext(String s) {
+            int[] next = new int[s.length()];
+            int j = 0;
+            for (int i = 1; i < s.length(); i++) {
+                if (j > 0 && s.charAt(i) != s.charAt(j)) {
+                    j = next[j - 1];
+                }
+                if (s.charAt(i) == s.charAt(j)) {
+                    j++;
+                }
+                next[i] = j;
+            }
+            return next;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
