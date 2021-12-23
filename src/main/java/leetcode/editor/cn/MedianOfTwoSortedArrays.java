055e9cc2ee04ef53a75e911abf23919b00723182
zhangguodong <jelly_54@163.com>
2021-12-23 08:34:25 +0800

    leetcode.cn-数组:4 : 寻找两个正序数组的中位数

--- src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
+++ src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -84,6 +84,7 @@ public class MedianOfTwoSortedArrays {
             int len = len1 + len2;
             int l = 0, r = 0, idx1 = 0, idx2 = 0;
             for (int i = 0; i <= len / 2; i++) {
+                // 记录上一次的结果
                 l = r;
                 if (idx1 < len1 && (idx2 >= len2 || nums1[idx1] < nums2[idx2])) {
                     r = nums1[idx1++];
@@ -91,12 +92,49 @@ public class MedianOfTwoSortedArrays {
                     r = nums2[idx2++];
                 }
             }
+            // 两个数组加起来数量是偶数
             if ((len & 1) == 0) {
                 return (l + r) / 2.0;
+            }
+            return r;
+        }
+
+        /**
+         * 时间复杂度：每进行一次循环，我们就减少 k/2 个元素，
+         * 所以时间复杂度是 O(log(k)，而 k=(m+n)/2，所以最终的复杂也就是 O(log(m+n）)
+         * <p>
+         * 空间复杂度：虽然我们用到了递归，但是可以看到这个递归属于尾递归，所以编译器不需要不停地堆栈，
+         * 所以空间复杂度为 O(1)
+         */
+        private double method2(int[] nums1, int[] nums2) {
+            int n = nums1.length;
+            int m = nums2.length;
+            int left = (n + m + 1) / 2;
+            int right = (n + m + 2) / 2;
+            //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+            return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) +
+                    getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+        }
+
+        private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+            int len1 = end1 - start1 + 1;
+            int len2 = end2 - start2 + 1;
+            //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+            if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+            if (len1 == 0) return nums2[start2 + k - 1];
+
+            if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+            int i = start1 + Math.min(len1, k / 2) - 1;
+            int j = start2 + Math.min(len2, k / 2) - 1;
+
+            if (nums1[i] > nums2[j]) {
+                return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
             } else {
-                return r;
+                return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
             }
         }
+
     }
 //leetcode submit region end(Prohibit modification and deletion)
 
