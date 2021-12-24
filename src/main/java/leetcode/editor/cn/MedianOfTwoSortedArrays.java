25bf0ce4665ace0e06653f2c16a84eb3f35bf2bd
zhangguodong <jelly_54@163.com>
2021-12-24 08:45:38 +0800

    leetcode.cn-数组:4 : 寻找两个正序数组的中位数

--- src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
+++ src/main/java/leetcode/editor/cn/MedianOfTwoSortedArrays.java
@@ -59,6 +59,7 @@ package leetcode.editor.cn;
 
 /**
  * title: 4 : 寻找两个正序数组的中位数
+ * 题解：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
  * since: 2021-12-21 07:43:45
  */
 public class MedianOfTwoSortedArrays {
@@ -135,7 +136,55 @@ public class MedianOfTwoSortedArrays {
             }
         }
 
+        /**
+         * 时间复杂度：我们对较短的数组进行了二分查找，所以时间复杂度是 O(log（min（m，n））
+         * <p>
+         * 空间复杂度：只有一些固定的变量，和数组长度无关，所以空间复杂度是 O ( 1 )
+         */
+        private double method3(int[] A, int[] B) {
+            int m = A.length;
+            int n = B.length;
+            if (m > n) {
+                return findMedianSortedArrays(B, A); // 保证 m <= n
+            }
+            int iMin = 0, iMax = m;
+            while (iMin <= iMax) {
+                int i = (iMin + iMax) / 2;
+                int j = (m + n + 1) / 2 - i;
+                if (j != 0 && i != m && B[j - 1] > A[i]) { // i 需要增大
+                    iMin = i + 1;
+                } else if (i != 0 && j != n && A[i - 1] > B[j]) { // i 需要减小
+                    iMax = i - 1;
+                } else { // 达到要求，并且将边界条件列出来单独考虑
+                    int maxLeft = 0;
+                    if (i == 0) {
+                        maxLeft = B[j - 1];
+                    } else if (j == 0) {
+                        maxLeft = A[i - 1];
+                    } else {
+                        maxLeft = Math.max(A[i - 1], B[j - 1]);
+                    }
+                    if ((m + n) % 2 == 1) {
+                        return maxLeft;
+                    } // 奇数的话不需要考虑右半部分
+
+                    int minRight = 0;
+                    if (i == m) {
+                        minRight = B[j];
+                    } else if (j == n) {
+                        minRight = A[i];
+                    } else {
+                        minRight = Math.min(B[j], A[i]);
+                    }
+
+                    return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+                }
+            }
+            return 0.0;
+        }
     }
+
+}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
