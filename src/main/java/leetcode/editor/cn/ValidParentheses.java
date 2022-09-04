1b377bf79e7ac4f126069788a68f640478351d1c
zhangguodong <jelly_54@163.com>
2022-09-04 11:29:20 +0800

    leetcode.cn-字符串:20 : 有效的括号

---
+++ src/main/java/leetcode/editor/cn/ValidParentheses.java
@@ -0,0 +1,88 @@
+//给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。 
+//
+// 有效字符串需满足： 
+//
+// 
+// 左括号必须用相同类型的右括号闭合。 
+// 左括号必须以正确的顺序闭合。 
+// 每个右括号都有一个对应的相同类型的左括号。 
+// 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "()"
+//输出：true
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = "()[]{}"
+//输出：true
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "(]"
+//输出：false
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 10⁴ 
+// s 仅由括号 '()[]{}' 组成 
+// 
+//
+// Related Topics 栈 字符串 👍 3477 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Stack;
+
+/**
+ * title: 20 : 有效的括号
+ * create: 2022-09-04 11:19:38
+ */
+public class ValidParentheses {
+    public static void main(String[] args) {
+        Solution solution = new ValidParentheses().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public boolean isValid(String s) {
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(' || c == '[' || c == '{') {
+                    stack.push(c);
+                } else {
+                    char need;
+                    if (c == ')') {
+                        need = '(';
+                    } else if (c == ']') {
+                        need = '[';
+                    } else {
+                        need = '{';
+                    }
+
+                    if (!stack.isEmpty() && need == stack.peek()) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            return stack.isEmpty();
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
