7a75e9efcdc2b06e3e86fd7e428eecd13121f00a
张国栋 <zhangguodong@songguo7.com>
2022-09-06 19:33:29 +0800

    leetcode.cn-单调栈:739 : 每日温度

---
+++ src/main/java/leetcode/editor/cn/DailyTemperatures.java
@@ -0,0 +1,73 @@
+//给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现
+//在几天后。如果气温在这之后都不会升高，请在该位置用 0 来代替。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: temperatures = [73,74,75,71,69,72,76,73]
+//输出: [1,1,4,2,1,1,0,0]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: temperatures = [30,40,50,60]
+//输出: [1,1,1,0]
+// 
+//
+// 示例 3: 
+//
+// 
+//输入: temperatures = [30,60,90]
+//输出: [1,1,0] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= temperatures.length <= 10⁵ 
+// 30 <= temperatures[i] <= 100 
+// 
+//
+// Related Topics 栈 数组 单调栈 👍 1275 👎 0
+
+package leetcode.editor.cn;
+
+import java.util.Stack;
+
+/**
+ * title: 739 : 每日温度
+ * create: 2022-09-06 19:27:18
+ */
+public class DailyTemperatures {
+    public static void main(String[] args) {
+        Solution solution = new DailyTemperatures().new Solution();
+        int[] ints = solution.dailyTemperatures(new int[]{30, 40, 50, 60});
+
+        for (int i : ints) {
+            System.out.print(i + ", ");
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] dailyTemperatures(int[] temperatures) {
+            int[] res = new int[temperatures.length];
+            Stack<Integer> stack = new Stack<>();
+
+            for (int i = temperatures.length - 1; i >= 0; i--) {
+                while (!stack.isEmpty() && temperatures[stack.peek()] <= temperatures[i]) {
+                    stack.pop();
+                }
+                res[i] = stack.isEmpty() ? 0 : (stack.peek() - i);
+                stack.push(i);
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
